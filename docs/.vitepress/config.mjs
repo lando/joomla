2bbb7a4fc88ff8b8f0f33a88b6b1b42264c90d25
Alec Reynolds <alec+git@lando.dev>
2024-02-01 20:17:01 -0800

    Sidebar collapse.

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -38,7 +38,7 @@ function sidebar() {
     },
     {
       text: 'Contribution',
-      collapsed: false,
+      collapsed: true,
       items: [
         {text: 'Development', link: '/development'},
         {text: 'Team', link: '/team'},
@@ -46,7 +46,7 @@ function sidebar() {
     },
     {
       text: 'Help & Support',
-      collapsed: false,
+      collapsed: true,
       items: [
         {text: 'GitHub', link: 'https://github.com/lando/joomla/issues/new/choose'},
         {text: 'Slack', link: 'https://www.launchpass.com/devwithlando'},
