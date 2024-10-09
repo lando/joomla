d489fe2324308f9e8cf48707aeac8ae4d773e991
Alec Reynolds <alec+git@lando.dev>
2024-10-09 16:27:05 -0700

    Update docs build to create multi-version builds. (#50)
    * Update docs build to create multi-version builds.
    
    * Build all doc versions from 1.0.0.

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -19,6 +19,9 @@ export default defineConfig({
     ['link', {rel: 'icon', href: '/joomla/favicon.svg', type: 'image/svg+xml'}],
   ],
   themeConfig: {
+    multiVersionBuild: {
+      satisfies: '>=1.0.0',
+    },
     sidebar: sidebar(),
   },
 });
